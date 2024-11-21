2f489aacc0f8cb3634a070734062f54b8de17a2b
Caden Erickson <caden.t.erickson@gmail.com>
2024-11-21 02:24:55 -0700

    Added Search endpoint
    - For now, it just supports exact textual match and returns titles that contain the query text

---
+++ Bookworms-Server/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using BookwormsServer.Models.Data;
+using BookwormsServer.Models.Entities;
+using BookwormsServer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace BookwormsServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SearchController(AllBookwormsDbContext dbContext, IBookApiService bookApiService) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult Search(string query)
+    {
+        List<Book> books = dbContext.Books
+            .Where(b => b.Title.Contains(query))
+            .ToList();
+        
+        Console.WriteLine(books.Count);
+        
+        List<BookDto> bookDtos = [];
+        bookDtos.AddRange(
+            from book in books
+            let bookDetails = JObject.Parse(bookApiService.GetData(book.BookId))
+            select BookDto.From(book, bookDetails)
+        );
+
+        return Ok(bookDtos);
+    }
+}
---
+++ Bookworms-Server/Models/Data/Book.cs
@@ -0,0 +1,18 @@
+﻿using BookwormsServer.Models.Entities;
+using Newtonsoft.Json.Linq;
+
+namespace BookwormsServer.Models.Data;
+
+public record BookDto(string Title, List<string> Authors, double Rating, double Difficulty, string Image)
+{
+    public static BookDto From(Book book, JObject bookDetails)
+    {
+        string title = book.Title;
+        List<string> authors = book.Authors;
+        double rating = book.StarRating ?? -1.0;
+        double difficulty = 0.0;
+        string image = bookDetails["image"]!.ToObject<string>() ?? "";
+        
+        return new BookDto(title, authors, rating, difficulty, image);
+    }
+}
