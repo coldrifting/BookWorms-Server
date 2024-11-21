014fa1556f37fb66c9625691a6751afd16f2fa4e
Caden Erickson <caden.t.erickson@gmail.com>
2024-11-21 14:40:25 -0700

    Change 'difficulty' in BookDTO from double to string

--- Bookworms-Server/Models/Data/Book.cs
+++ Bookworms-Server/Models/Data/Book.cs
@@ -3,14 +3,14 @@ using Newtonsoft.Json.Linq;
 
 namespace BookwormsServer.Models.Data;
 
-public record BookDto(string Title, List<string> Authors, double Rating, double Difficulty, string Image)
+public record BookDto(string Title, List<string> Authors, double Rating, string Difficulty, string Image)
 {
     public static BookDto From(Book book, JObject bookDetails)
     {
         string title = book.Title;
         List<string> authors = book.Authors;
         double rating = book.StarRating ?? -1.0;
-        double difficulty = 0.0;
+        string difficulty = book.Level ?? "";
         string image = bookDetails["image"]!.ToObject<string>() ?? "";
         
         return new BookDto(title, authors, rating, difficulty, image);
