976b436135d97da19213e046d35a65435fd4e906
Aiden Van Dyke <u0964239@utah.edu>
2025-02-01 01:03:11 +0000

    Cleaned up seeding code via generic method
    * Cleaned up seeding code via generic method
    * Sorted order of DB set variables in context class to match order of FK relations

--- Bookworms-Server/Models/BookwormsDbContext.cs
+++ Bookworms-Server/Models/BookwormsDbContext.cs
@@ -7,85 +7,70 @@ namespace BookwormsServer;
 
 public class BookwormsDbContext(DbContextOptions<BookwormsDbContext> options) : DbContext(options)
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Parent> Parents { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    
     public DbSet<Book> Books { get; set; }
-    public DbSet<Bookshelf> Bookshelves { get; set; }
-    public DbSet<BookshelfBook> BookshelfBooks { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    
     public DbSet<Child> Children { get; set; }
     public DbSet<ChildBookshelf> ChildBookshelves { get; set; }
-    public DbSet<Classroom> Classrooms { get; set; }
-    public DbSet<ClassroomBookshelf> ClassroomBookshelves { get; set; }
+    public DbSet<Bookshelf> Bookshelves { get; set; }
+    public DbSet<BookshelfBook> BookshelfBooks { get; set; }
+    
     public DbSet<CompletedBookshelf> CompletedBookshelves { get; set; }
     public DbSet<InProgressBookshelf> InProgressBookshelves { get; set; }
-    public DbSet<Parent> Parents { get; set; }
-    public DbSet<Review> Reviews { get; set; }
-    public DbSet<Teacher> Teachers { get; set; }
-    public DbSet<User> Users { get; set; }
+    
+    public DbSet<Classroom> Classrooms { get; set; }
+    public DbSet<ClassroomBookshelf> ClassroomBookshelves { get; set; }
+
+    readonly JsonSerializerOptions _jso = new()
+    {
+	    PropertyNameCaseInsensitive = true,
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
     
     public void SeedTestData()
     {
-	    JsonSerializerOptions jso = new()
-	    {
-		    PropertyNameCaseInsensitive = true,
-			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-			Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
-	    };
+	    Clear();
 
-	    // Database.EnsureDeleted();
-        // Database.Migrate();
-		Clear();
-        
-	    string userData = File.ReadAllText("TestData/UserEntities.json");
-		var users = JsonSerializer.Deserialize<List<User>>(userData, jso)!;
-		Users.AddRange(users);
-		SaveChanges();
-		
-	    string parentData = File.ReadAllText("TestData/ParentEntities.json");
-		var parents = JsonSerializer.Deserialize<List<Parent>>(parentData, jso)!;
-		Parents.AddRange(parents);
-		SaveChanges();
-
-	    string teacherData = File.ReadAllText("TestData/TeacherEntities.json");
-		var teachers = JsonSerializer.Deserialize<List<Teacher>>(teacherData, jso)!;
-		Teachers.AddRange(teachers);
-		SaveChanges();
-		
-	    string bookData = File.ReadAllText("TestData/BookEntities.json");
-		var books = JsonSerializer.Deserialize<List<Book>>(bookData, jso)!;
-		Books.AddRange(books);
-		SaveChanges();
-		
-	    string reviewData = File.ReadAllText("TestData/ReviewEntities.json");
-		var reviews = JsonSerializer.Deserialize<List<Review>>(reviewData, jso)!;
-		Reviews.AddRange(reviews);
-		SaveChanges();
-		
-	    string childData = File.ReadAllText("TestData/ChildEntities.json");
-		var children = JsonSerializer.Deserialize<List<Child>>(childData, jso)!;
-		Children.AddRange(children);
-		SaveChanges();
-
-		string childBookshelfData = File.ReadAllText("TestData/ChildBookshelfEntities.json");
-		var childBookshelf = JsonSerializer.Deserialize<List<ChildBookshelf>>(childBookshelfData, jso)!;
-		ChildBookshelves.AddRange(childBookshelf);
-		SaveChanges();
-
-		string bookshelfBookData = File.ReadAllText("TestData/BookshelfBookEntities.json");
-		var bookshelfBooks = JsonSerializer.Deserialize<List<BookshelfBook>>(bookshelfBookData, jso)!;
-		BookshelfBooks.AddRange(bookshelfBooks);
-		SaveChanges();
-		
-		SaveChanges();
+	    Seed<User>();
+	    Seed<Parent>();
+	    Seed<Teacher>();
+	    
+	    Seed<Book>();
+	    Seed<Review>();
+		    
+	    Seed<Child>();
+	    Seed<ChildBookshelf>();
+	    Seed<BookshelfBook>();
     }
 
     public void Clear()
     {
 	    Database.ExecuteSqlRaw("DELETE FROM BookshelfBooks;");
-	    Database.ExecuteSqlRaw("DELETE FROM Bookshelves;");
-	    Database.ExecuteSqlRaw("DELETE FROM ChildBookshelves;");
-	    Database.ExecuteSqlRaw("DELETE FROM Children;");
-	    Database.ExecuteSqlRaw("DELETE FROM Reviews;");
-	    Database.ExecuteSqlRaw("DELETE FROM Books;");
-	    Database.ExecuteSqlRaw("DELETE FROM Users;");
+		Database.ExecuteSqlRaw("DELETE FROM Bookshelves;");
+		Database.ExecuteSqlRaw("DELETE FROM ChildBookshelves;");
+		Database.ExecuteSqlRaw("DELETE FROM Children;");
+		Database.ExecuteSqlRaw("DELETE FROM Reviews;");
+		Database.ExecuteSqlRaw("DELETE FROM Books;");
+		Database.ExecuteSqlRaw("DELETE FROM Users;");
     }
     
+    private void Seed<T>() where T : class
+    {
+	    string typeName = typeof(T).Name;
+
+	    string data = File.ReadAllText("TestData" + Path.DirectorySeparatorChar + typeName + "Entities.json");
+		List<T>? objects = JsonSerializer.Deserialize<List<T>>(data, _jso);
+		if (objects is null)
+		{
+			return;
+		}
+
+		Set<T>().AddRange(objects);
+		SaveChanges();
+    }
 }
